ded5b1e4e09242ee4c4afe92a10882e37aaf94d8
邓旭文 <xwd0823@163.com>
2021-09-04 23:46:55 +0800

    add ch4

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 [1. 入门](./notes/1.start.md) <br>
 [2. 程序结构](./notes/02.program-structure.md) <br>
 [3. 基本数据类型](./notes/03.basic-type.md) <br>
+[4. 复合数据类型](./notes/04.compound-type.md) <br>
 [10. 包管理](./notes/10.package.md) <br>
---
+++ ch4/sha256.go
@@ -0,0 +1,13 @@
+// 使用 SHA256 加密散列算法生成一个摘要
+package main
+
+import (
+	"fmt"
+	"crypto/sha256"
+)
+
+func main() {
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+}
